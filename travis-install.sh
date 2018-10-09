9abbd949093d39e39c48258ff2457dc21d6b0b36
Eason Chen <easoncxz@gmail.com>
2018-10-09 23:39:44 +1300

    Immitate hack-assembler setup

--- travis-install.sh
+++ travis-install.sh
@@ -3,15 +3,31 @@
 set -e  # fail fast
 set -x  # echo on
 
-uname -a
-hostname
-whoami
-pwd
+# Bring Homebrew up to date, ...
+brew doctor || true
+brew update || true
+
+# ... so that we can install GnuPG, ...
+brew install gnupg || brew upgrade gnupg || which gpg
+echo 'Expecting we have GnuPG v2.x'
+gpg --version
+gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+# ... so that we can verify the new RVM binary when we download it, ...
+rvm get stable
+
+# ... so that we can have a version of Ruby that works with homebrew_automation, ...
+# rvm osx-ssl-certs update all    # https://github.com/rubygems/rubygems.org/issues/613
+rvm install "$(cat .ruby-version)"
+rvm use  "$(cat .ruby-version)"
+
 
 which -a ruby
 which -a gem
 ruby --version
 gem --version
 
-brew doctor || true
-brew update || true
+gem install homebrew_automation -v 0.0.8
+
+#brew doctor || true
+#brew update || true
