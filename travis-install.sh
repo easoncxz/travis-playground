c994876fba8f920793e481c5633d910fc50f7a67
Eason Chen <easoncxz@gmail.com>
2018-10-12 18:20:36 +1300

    Try cache on non-Ruby stuff instead

--- travis-install.sh
+++ travis-install.sh
@@ -11,37 +11,46 @@ brew doctor || true
 brew upgrade || true
 brew --version
 
-function with_echo {
-    echo "$*";
-    # Crucially, without quotes:
-    $*
-}
+#function with_echo {
+#    echo "$*";
+#    # Crucially, without quotes:
+#    $*
+#}
+#
+#set +x  # RVM is really too noisy
+#with_echo rvm version
+#with_echo rvm get stable
+#with_echo rvm version
+#
+#with_echo rvm list
+#with_echo rvm disk-usage all
+#
+#with_echo cat .ruby-version
+#with_echo rvm use "$(cat .ruby-version)" --install
+#
+#echo 'Uninstalling other Rubies...'
+#other_rubies="$(comm -23 <(rvm list strings) <(cat .ruby-version))"
+#echo "Other Rubies:"
+#echo "$other_rubies"
+#echo "$other_rubies" | xargs -n 1 rvm uninstall
+#
+#with_echo rvm cleanup all
+#with_echo rvm list
+#with_echo rvm disk-usage all
+#set -x
+#
+#which -a ruby
+#which -a gem
+#ruby --version
+#gem --version
+#
+#gem install homebrew_automation -v 0.0.8
 
-set +x  # RVM is really too noisy
-with_echo rvm version
-with_echo rvm get stable
-with_echo rvm version
-
-with_echo rvm list
-with_echo rvm disk-usage all
-
-with_echo cat .ruby-version
-with_echo rvm use "$(cat .ruby-version)" --install
-
-echo 'Uninstalling other Rubies...'
-other_rubies="$(comm -23 <(rvm list strings) <(cat .ruby-version))"
-echo "Other Rubies:"
-echo "$other_rubies"
-echo "$other_rubies" | xargs -n 1 rvm uninstall
-
-with_echo rvm cleanup all
-with_echo rvm list
-with_echo rvm disk-usage all
-set -x
-
-which -a ruby
-which -a gem
-ruby --version
-gem --version
-
-gem install homebrew_automation -v 0.0.8
+# Install Stack binary
+if [ -d ~/.stack ]
+then
+    rm -rf ~/.stack
+fi
+which -a stack || ( curl -sSL https://get.haskellstack.org/ | sh )
+stack --resolver 'lts-12.12' install hlint
+du -h -d 2 ~/.stack
